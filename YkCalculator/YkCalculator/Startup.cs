85b699117d12a13804356982c481bfc861a470de
Daniel Foo <danielvundy@gmail.com>
2020-02-16 15:24:41 +0800

    Added CORS support

--- YkCalculator/YkCalculator/Startup.cs
+++ YkCalculator/YkCalculator/Startup.cs
@@ -26,6 +26,14 @@ namespace YkCalculator
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowSpecificOrigin",
+                    builder => builder.WithOrigins("*").
+                    AllowAnyHeader().AllowAnyMethod()
+                    );
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
