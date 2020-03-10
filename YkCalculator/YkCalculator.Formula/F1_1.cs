1f3b056d82980d95757c5acca624d3909c5697b6
Daniel Foo <danielvundy@gmail.com>
2020-03-10 21:56:31 +0800

    Update F1_1 formula to use input harga cincin

--- YkCalculator/YkCalculator.Formula/F1_1.cs
+++ YkCalculator/YkCalculator.Formula/F1_1.cs
@@ -11,10 +11,10 @@ namespace YkCalculator.Formula
             {
                 Input = input
             };
-
+            
             result.Keping = (int)Math.Ceiling(input.Lebar / 28.0) * input.Set;
             result.Upah1 = Math.Round((double)result.Keping * 3, 2);
-            result.Upah2Cincin = Math.Round(result.Keping * 11.2, 2);
+            result.Upah2Cincin = Math.Round(result.Keping * input.HargaCincin, 2);
             result.Kain = Math.Round((double)(input.Tinggi + 15) / 39 * result.Keping * input.HargaKainA, 2);
             result.Jumlah = Math.Round(result.Upah1 + result.Upah2Cincin + result.Kain, 2);
 
