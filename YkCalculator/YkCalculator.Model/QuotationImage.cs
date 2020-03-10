7692a6882505808bdb1ffa528af204ab55c2aed3
Daniel Foo <danielvundy@gmail.com>
2020-03-10 22:43:59 +0800

    Implement PUT endpoint to update image for quotation

---
+++ YkCalculator/YkCalculator.Model/QuotationImage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YkCalculator.Model
+{
+    public class QuotationImage
+    {
+        public List<string> Image { get; set; }
+    }
+}
---
+++ YkCalculator/YkCalculator/Controllers/QuotationController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using YkCalculator.DAL;
+using YkCalculator.Model;
+
+namespace YkCalculator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuotationController : ControllerBase
+    {
+        // PUT: api/Quotation/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] QuotationImage quotationImage)
+        {
+            Quotation quotation = new Quotation();
+            quotation.Update(id, quotationImage.Image);
+        }
+    }
+}
