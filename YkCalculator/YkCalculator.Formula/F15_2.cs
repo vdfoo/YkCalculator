4a0507f335fb8bd0b2a7cfbf5fb6ebdeee71060f
Daniel Foo <danielvundy@gmail.com>
2020-03-24 21:52:39 +0800

    F15_2 implementation

---
+++ YkCalculator/YkCalculator.Formula/F15_2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using YkCalculator.Model;
+
+namespace YkCalculator.Logic
+{
+    public class F15_2 : FormulaBase, IFormula
+    {
+        public Output Calculate(Input input)
+        {
+            Output result = new Output()
+            {
+                Input = input
+            };
+
+            result.Keping = (int)Math.Ceiling(input.Lebar / 28.0) * input.Set;
+            result.UpahKainA = Math.Round((double)result.Keping * 3, 2);
+            result.HargaKainA = Math.Round((double)(input.Lebar + 10) / 39 * input.HargaKainA * result.Keping / 2, 2);
+            result.UpahHook = Math.Round((double)input.HargaHook * result.Keping, 2);
+            result.Jumlah = Math.Round(result.UpahKainA + result.HargaKainA + result.UpahHook, 2);
+
+            result.TailorKeping = result.Keping;
+            result.TailorTinggi = Math.Round((double)(input.Lebar + 5) / 39, 2);
+            result.TailorTotal = result.TailorKeping;
+
+            return result;
+        }
+    }
+}
--- YkCalculator/YkCalculator.Formula/FormulaManager.cs
+++ YkCalculator/YkCalculator.Formula/FormulaManager.cs
@@ -47,7 +47,8 @@ namespace YkCalculator.Logic
                     formula = new F13_2(); break;
                 case "F15_1":
                     formula = new F15_1(); break;
-
+                case "F15_2":
+                    formula = new F15_2(); break;
                 default:
                     throw new NotImplementedException($"No implementation for formula {input.FormulaCode}");
             }
