c63397a7a6a39cddfea431a931e0a95a80ca5632
Daniel Foo <danielvundy@gmail.com>
2020-03-29 22:51:11 +0800

    TailorRendaKeping update

--- YkCalculator/YkCalculator.Formula/F29_1.cs
+++ YkCalculator/YkCalculator.Formula/F29_1.cs
@@ -32,7 +32,7 @@ namespace YkCalculator.Logic
             result.TailorTinggi = Math.Round((24 + 5) / 39.0, 2);
             result.TailorTinggiB = Math.Round((input.Lebar + 10) / 39.0, 2);
             result.TailorRenda = Math.Round((56 * 10 + 5) / 39.0, 2);
-            result.TailorRendaKeping = result.Keping / 5;
+            result.TailorRendaKeping = Math.Round(result.Keping / 5.0, 2);
             result.TailorTotal = result.TailorKeping + result.KepingB;
 
             return result;
