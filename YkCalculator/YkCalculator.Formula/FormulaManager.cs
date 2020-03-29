92ec4b54f0dd8a8041033cbefeef90a5cc1ecf7d
Daniel Foo <danielvundy@gmail.com>
2020-03-29 16:35:42 +0800

    Apply reflection resolving formula class

--- YkCalculator/YkCalculator.Formula/FormulaManager.cs
+++ YkCalculator/YkCalculator.Formula/FormulaManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
 using YkCalculator.Model;
 
@@ -9,80 +10,19 @@ namespace YkCalculator.Logic
     {
         public Output Identify(Input input)
         {
-            Output result;
-            IFormula formula;
-            switch (input.FormulaCode)
-            {
-                case "F1_1":
-                    formula = new F1_1(); break;
-                case "F1_2":
-                    formula = new F1_2(); break;
-                case "F3_1":
-                    formula = new F3_1(); break;
-                case "F3_2":
-                    formula = new F3_2(); break;
-                case "F3_3":
-                    formula = new F3_3(); break;
-                case "F3_4":
-                    formula = new F3_4(); break;
-                case "F5_1":
-                    formula = new F5_1(); break;
-                case "F5_2":
-                    formula = new F5_2(); break;
-                case "F7_1":
-                    formula = new F7_1(); break;
-                case "F7_2":
-                    formula = new F7_2(); break;
-                case "F9_1":
-                    formula = new F9_1(); break;
-                case "F9_2": 
-                    formula = new F9_2(); break;
-                case "F11_1":
-                    formula = new F11_1(); break;
-                case "F11_2":
-                    formula = new F11_2(); break;
-                case "F13_1":
-                    formula = new F13_1(); break;
-                case "F13_2":
-                    formula = new F13_2(); break;
-                case "F15_1":
-                    formula = new F15_1(); break;
-                case "F15_2":
-                    formula = new F15_2(); break;
-                case "F17_1":
-                    formula = new F17_1(); break;
-                case "F17_2":
-                    formula = new F17_2(); break;
-                case "F17_3":
-                    formula = new F17_3(); break;
-                case "F19_1":
-                    formula = new F19_1(); break;
-                case "F19_2":
-                    formula = new F19_2(); break;
-                case "F21_1":
-                    formula = new F21_1(); break;
-                case "F21_2":
-                    formula = new F21_2(); break;
-                case "F23_1":
-                    formula = new F23_1(); break;
-                case "F25_1":
-                    formula = new F25_1(); break;
-                case "F25_2":
-                    formula = new F25_2(); break;
-                case "F27_1_1":
-                    formula = new F27_1_1(); break;
-                case "F27_1_2":
-                    formula = new F27_1_2(); break;
-                case "F27_2_1":
-                    formula = new F27_2_1(); break;
-                case "F27_2_2":
-                    formula = new F27_2_2(); break;
-                default:
-                    throw new NotImplementedException($"No implementation for formula {input.FormulaCode}");
+            try
+            { 
+                Type type = Type.GetType("YkCalculator.Logic." + input.FormulaCode);
+                ConstructorInfo constructor = type.GetConstructor(Type.EmptyTypes);
+                object classObject = constructor.Invoke(new object[] { });
+                MethodInfo method = type.GetMethod("Calculate");
+                Output result = (Output)method.Invoke(classObject, new object[] { input });
+                return result;
             }
-
-            result = formula.Calculate(input);
-            return result; 
+            catch (Exception ex)
+            {
+                throw new Exception($"{ex.Message} {Environment.NewLine} Could not resolve {input.FormulaCode}");
+            }
         }
     }
 }
