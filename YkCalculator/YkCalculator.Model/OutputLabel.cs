fdfe1c4d3229e477a69c081a6fd40d47ea88ced3
Daniel Foo <danielvundy@gmail.com>
2020-04-13 21:32:08 +0800

    Generic output label

--- YkCalculator/YkCalculator.Model/OutputLabel.cs
+++ YkCalculator/YkCalculator.Model/OutputLabel.cs
@@ -10,9 +10,6 @@ namespace YkCalculator.Model
         {
             FormulaCode = formulaCode;
             Fields = new Dictionary<string, string>();
-
-            Fields.Add("formulaCode", "Kod");
-            Fields.Add("jumlah", "Jumlah");
         }
         public string FormulaCode { get; set; }
         public Dictionary<string, string> Fields { get; set; }
--- YkCalculator/YkCalculator.Model/OutputLabelCollection.cs
+++ YkCalculator/YkCalculator.Model/OutputLabelCollection.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
 
 namespace YkCalculator.Model
@@ -11,97 +12,65 @@ namespace YkCalculator.Model
         public OutputLabelCollection()
         {
             Formula = new List<OutputLabel>();
+            
 
-            OutputLabel label = new OutputLabel("F1_1");
-            label.Fields.Add("keping", "Keping");
-            label.Fields.Add("upahKainA", "Upah Kain A");
-            label.Fields.Add("upahCincin", "Upah Cincin");
-            label.Fields.Add("hargaKainA", "Harga Kain A");
-            label.Fields.Add("tailorKeping", "Tailor Keping");
-            label.Fields.Add("tailorTinggi", "Tailor Tinggi");
-            label.Fields.Add("tailorTotalKeping", "Tailor Total Keping");
+            OutputLabel label = new OutputLabel(string.Empty);
+
+            label.Fields.Add(ToLowerFirstChar(nameof(Output.HargaButang)), "Harga Butang");
+            label.Fields.Add(ToLowerFirstChar(nameof(Output.HargaCincin)), "Harga Cincin");
+            label.Fields.Add(ToLowerFirstChar(nameof(Output.HargaKainA)), "Harga Kain A");
+            label.Fields.Add(ToLowerFirstChar(nameof(Output.HargaKainB)), "Harga Kain B");
+            label.Fields.Add(ToLowerFirstChar(nameof(Output.HargaKainC)), "Harga Kain C");
+            label.Fields.Add(ToLowerFirstChar(nameof(Output.HargaKainD)), "Harga Kain D");
+            label.Fields.Add(ToLowerFirstChar(nameof(Output.HargaRainbow)), "Harga Rainbow");
+            label.Fields.Add(ToLowerFirstChar(nameof(Output.HargaRenda)), "Harga Renda");
+            label.Fields.Add(ToLowerFirstChar(nameof(Output.HargaRenda2)), "Harga Renda 2");
+            label.Fields.Add(ToLowerFirstChar(nameof(Output.HargaRenda3)), "Harga Renda 3");
+            label.Fields.Add(ToLowerFirstChar(nameof(Output.HargaRenda4)), "Harga Renda 4");
+            label.Fields.Add(ToLowerFirstChar(nameof(Output.HargaRibbon)), "Harga Ribbon");
+            label.Fields.Add(ToLowerFirstChar(nameof(Output.HargaTali)), "Harga Tali");
+            label.Fields.Add(ToLowerFirstChar(nameof(Output.Image)), "Gambar");
+            label.Fields.Add(ToLowerFirstChar(nameof(Output.Jumlah)), "Jumlah");
+            label.Fields.Add(ToLowerFirstChar(nameof(Output.JumlahA)), "Jumlah A");
+            label.Fields.Add(ToLowerFirstChar(nameof(Output.JumlahB)), "Jumlah B");
+            label.Fields.Add(ToLowerFirstChar(nameof(Output.Keping)), "Keping");
+            label.Fields.Add(ToLowerFirstChar(nameof(Output.KepingA)), "KepingA");
+            label.Fields.Add(ToLowerFirstChar(nameof(Output.KepingB)), "KepingB");
+            label.Fields.Add(ToLowerFirstChar(nameof(Output.QuotationId)), "Quotation ID");
+            label.Fields.Add(ToLowerFirstChar(nameof(Output.TailorJalur)), "Tailor Jalur");
+            label.Fields.Add(ToLowerFirstChar(nameof(Output.TailorKeping)), "Tailor Keping");
+            label.Fields.Add(ToLowerFirstChar(nameof(Output.TailorMeterA)), "Tailor Meter A");
+            label.Fields.Add(ToLowerFirstChar(nameof(Output.TailorMeterB)), "Tailor Meter B");
+            label.Fields.Add(ToLowerFirstChar(nameof(Output.TailorMeterC)), "Tailor Meter C");
+            label.Fields.Add(ToLowerFirstChar(nameof(Output.TailorMeterD)), "Tailor Meter D");
+            label.Fields.Add(ToLowerFirstChar(nameof(Output.TailorMeterG)), "Tailor Meter G");
+            label.Fields.Add(ToLowerFirstChar(nameof(Output.TailorMeterK)), "Tailor Meter K");
+            label.Fields.Add(ToLowerFirstChar(nameof(Output.TailorRenda)), "Tailor Renda");
+            label.Fields.Add(ToLowerFirstChar(nameof(Output.TailorRenda2)), "Tailor Renda 2");
+            label.Fields.Add(ToLowerFirstChar(nameof(Output.TailorRenda3)), "Tailor Renda 3");
+            label.Fields.Add(ToLowerFirstChar(nameof(Output.TailorRenda4)), "Tailor Renda 4");
+            label.Fields.Add(ToLowerFirstChar(nameof(Output.TailorRendaKeping)), "Tailor Renda Keping");
+            label.Fields.Add(ToLowerFirstChar(nameof(Output.TailorTinggi)), "Tailor Tinggi");
+            label.Fields.Add(ToLowerFirstChar(nameof(Output.TailorTinggiB)), "Tailor Tinggi B");
+            label.Fields.Add(ToLowerFirstChar(nameof(Output.TailorTotalKeping)), "Tailor Keping Jumlah");
+            label.Fields.Add(ToLowerFirstChar(nameof(Output.UpahButang)), "Upah Butang");
+            label.Fields.Add(ToLowerFirstChar(nameof(Output.UpahHook)), "Upah Hook");
+            label.Fields.Add(ToLowerFirstChar(nameof(Output.UpahKainA)), "Upah Kain");
+            label.Fields.Add(ToLowerFirstChar(nameof(Output.UpahKainB)), "Upah Kain B");
+
             Formula.Add(label);
 
-            label = new OutputLabel("F1_2");
-            label.Fields.Add("keping", "Keping");
-            label.Fields.Add("upahKainA", "Upah Kain A");
-            label.Fields.Add("upahCincin", "Upah Cincin");
-            label.Fields.Add("hargaKainA", "Harga Kain A");
-            label.Fields.Add("tailorKeping", "Tailor Keping");
-            label.Fields.Add("tailorTinggi", "Tailor Tinggi");
-            label.Fields.Add("tailorTotalKeping", "Tailor Total Keping");
-            Formula.Add(label);
+        }
 
-            label = new OutputLabel("F3_1");
-            label.Fields.Add("keping", "Keping");
-            label.Fields.Add("upahKainA", "Upah Kain A");
-            label.Fields.Add("hargaKainA", "Harga Kain A");
-            label.Fields.Add("tailorKeping", "Tailor Keping");
-            label.Fields.Add("tailorTinggi", "Tailor Tinggi");
-            label.Fields.Add("tailorTotalKeping", "Tailor Total Keping");
-            Formula.Add(label);
+        public string ToLowerFirstChar(string s)
+        {
+            if (s != string.Empty && char.IsUpper(s[0]))
+            {
+                s = char.ToLower(s[0]) + s.Substring(1);
+            }
 
-            label = new OutputLabel("F3_2");
-            label.Fields.Add("keping", "Keping");
-            label.Fields.Add("upahKainA", "Upah Kain A");
-            label.Fields.Add("hargaKainA", "Harga Kain A");
-            label.Fields.Add("tailorKeping", "Tailor Keping");
-            label.Fields.Add("tailorTinggi", "Tailor Tinggi");
-            label.Fields.Add("tailorTotalKeping", "Tailor Total Keping");
-            Formula.Add(label);
-
-            label = new OutputLabel("F3_3");
-            label.Fields.Add("keping", "Keping");
-            label.Fields.Add("upahKainA", "Upah Kain A");
-            label.Fields.Add("hargaKainA", "Harga Kain A");
-            label.Fields.Add("tailorKeping", "Tailor Keping");
-            label.Fields.Add("tailorJalur", "Tailor Jalur");
-            label.Fields.Add("tailorTotalKeping", "Tailor Total Keping");
-            Formula.Add(label);
-
-            label = new OutputLabel("F3_4");
-            label.Fields.Add("keping", "Keping");
-            label.Fields.Add("upahKainA", "Upah Kain A");
-            label.Fields.Add("hargaKainA", "Harga Kain A");
-            label.Fields.Add("tailorKeping", "Tailor Keping");
-            label.Fields.Add("tailorTinggi", "Tailor Tinggi");
-            label.Fields.Add("tailorTotalKeping", "Tailor Total Keping");
-            Formula.Add(label);
-
-            label = new OutputLabel("F5_1");
-            label.Fields.Add("keping", "Keping");
-            label.Fields.Add("kepingB", "Keping B");
-            label.Fields.Add("upahKainA", "Upah Kain A");
-            label.Fields.Add("upahButang", "Upah Butang");
-            label.Fields.Add("upahHook", "Upah Hook");
-            label.Fields.Add("hargaKainA", "Harga Kain A");
-            label.Fields.Add("tailorKeping", "Tailor Keping");
-            label.Fields.Add("tailorTinggi", "Tailor Tinggi");
-            label.Fields.Add("tailorTotalKeping", "Tailor Total Keping");
-            Formula.Add(label);
-
-            label = new OutputLabel("F5_2");
-            label.Fields.Add("keping", "Keping");
-            label.Fields.Add("upahKainA", "Upah Kain A");
-            label.Fields.Add("upahButang", "Upah Butang");
-            label.Fields.Add("upahHook", "Upah Hook");
-            label.Fields.Add("hargaKainA", "Harga Kain A");
-            label.Fields.Add("tailorKeping", "Tailor Keping");
-            label.Fields.Add("tailorTinggi", "Tailor Tinggi");
-            label.Fields.Add("tailorTotalKeping", "Tailor Total Keping");
-            Formula.Add(label);
-
-            label = new OutputLabel("F7_1");
-            label.Fields.Add("keping", "Keping");
-            label.Fields.Add("kepingB", "Keping B");
-            label.Fields.Add("upahKainA", "Upah Kain A");
-            label.Fields.Add("upahButang", "Upah Butang");
-            label.Fields.Add("upahHook", "Upah Hook");
-            label.Fields.Add("hargaKainA", "Harga Kain A");
-            label.Fields.Add("tailorKeping", "Tailor Keping");
-            label.Fields.Add("tailorTinggi", "Tailor Tinggi");
-            label.Fields.Add("tailorTotalKeping", "Tailor Total Keping");
-            Formula.Add(label);
+            return s;
         }
     }
 }
+
