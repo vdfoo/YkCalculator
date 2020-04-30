46fabda43305ae11513bf618cbf1e155c4ea77c9
Daniel Foo <danielvundy@gmail.com>
2020-05-01 00:11:19 +0800

    Add RodSetOutput in F97 unit tests and Postman test

--- YkCalculator/YkCalculator.FormulaTests/F97_1Tests.cs
+++ YkCalculator/YkCalculator.FormulaTests/F97_1Tests.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using YkCalculator.Model;
+using System.Text.Json;
 
 namespace YkCalculator.Logic.Tests
 {
@@ -13,20 +14,44 @@ namespace YkCalculator.Logic.Tests
         [TestMethod()]
         public void CalculateTest()
         {
+            RodSetInput rodSetInput = new RodSetInput();
+            rodSetInput.FormulaCode = "F97";
+            rodSetInput.ReadyMadeProduct = new List<ReadyMadeProduct>();
+
+            ReadyMadeProduct p1 = new ReadyMadeProduct("F97.1", "Rod Kayu", string.Empty, 0)
+            {
+                Meter = 4,
+                Quantity = 2
+            };
+
+            ReadyMadeProduct p2 = new ReadyMadeProduct("F97.2", "Aluminium Rail", string.Empty, 0)
+            {
+                Meter = 3,
+                Quantity = 3
+            };
+
+            rodSetInput.ReadyMadeProduct.Add(p1);
+            rodSetInput.ReadyMadeProduct.Add(p2);
+
+            RodSetOutput rodSetOutput = new F97().Calculate(rodSetInput);
+
+            string jsonOutput = JsonSerializer.Serialize(rodSetOutput);
+
             Input input = new Input
             {
                 Set = 1,
                 HargaKainA = 12,
                 Lebar = 120,
                 Tinggi = 100,
-                Layout = "L"
+                Layout = "L",
+                RodSetOutput = rodSetOutput
             };
 
             IFormula formula = new F97_1();
             Output actual = formula.Calculate(input);
 
             Assert.AreEqual(actual.Keping, 5);
-            Assert.AreEqual(actual.Jumlah, 275.92);
+            Assert.AreEqual(actual.Jumlah, 569.92); // to confirm if need transportation 100
             Assert.AreEqual(actual.HargaKainA, 275.92);
             Assert.AreEqual(actual.TailorTotalKeping, 5);
         }
--- YkCalculator/YkCalculator.FormulaTests/F97_2_2Tests.cs
+++ YkCalculator/YkCalculator.FormulaTests/F97_2_2Tests.cs
@@ -13,20 +13,42 @@ namespace YkCalculator.Logic.Tests
         [TestMethod()]
         public void CalculateTest()
         {
+            RodSetInput rodSetInput = new RodSetInput();
+            rodSetInput.FormulaCode = "F97";
+            rodSetInput.ReadyMadeProduct = new List<ReadyMadeProduct>();
+
+            ReadyMadeProduct p1 = new ReadyMadeProduct("F97.1", "Rod Kayu", string.Empty, 0)
+            {
+                Meter = 4,
+                Quantity = 2
+            };
+
+            ReadyMadeProduct p2 = new ReadyMadeProduct("F97.2", "Aluminium Rail", string.Empty, 0)
+            {
+                Meter = 3,
+                Quantity = 3
+            };
+
+            rodSetInput.ReadyMadeProduct.Add(p1);
+            rodSetInput.ReadyMadeProduct.Add(p2);
+
+            RodSetOutput rodSetOutput = new F97().Calculate(rodSetInput);
+
             Input input = new Input
             {
                 Set = 1,
                 HargaKainA = 13,
                 Lebar = 120,
                 Tinggi = 100,
-                Layout = "L"
+                Layout = "L",
+                RodSetOutput = rodSetOutput
             };
 
             IFormula formula = new F97_2_2();
             Output actual = formula.Calculate(input);
 
             Assert.AreEqual(actual.Keping, 4);
-            Assert.AreEqual(actual.Jumlah, 181.20);
+            Assert.AreEqual(actual.Jumlah, 475.20);
             Assert.AreEqual(actual.HargaKainA, 181.20);
             Assert.AreEqual(actual.TailorTotalKeping, 4);
         }
