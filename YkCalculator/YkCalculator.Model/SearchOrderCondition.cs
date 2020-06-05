4c9f8865094dc2371f5eaa80dec7a9928014cc8d
Daniel Foo <danielvundy@gmail.com>
2020-06-05 19:43:45 +0800

    Initialize DateTo and DateFrom with valid date

--- YkCalculator/YkCalculator.Model/SearchOrderCondition.cs
+++ YkCalculator/YkCalculator.Model/SearchOrderCondition.cs
@@ -9,9 +9,16 @@ namespace YkCalculator.Model
         public string Username { get; set; }
         public DateTime DateFrom { get; set; }
         public DateTime DateTo { get; set; }
-        public int OrderIdFrom  { get; set; }
+        public int OrderIdFrom { get; set; }
         public int OrderIdTo { get; set; }
         public int OrderId { get; set; }
         public int Offset { get; set; }
+
+        public SearchOrderCondition()
+        {
+            DateFrom = DateTime.Parse("2020-01-01T00:00:00");
+            DateTo = DateTime.Parse("2050-01-01T00:00:00");
+        }
+
     }
 }
