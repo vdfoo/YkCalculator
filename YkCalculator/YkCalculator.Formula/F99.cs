e6c5770645af92fa2838e6698b52aa3a02e3288c
Daniel Foo <danielvundy@gmail.com>
2020-07-22 21:58:34 +0800

    F99 implementation

---
+++ YkCalculator/YkCalculator.Formula/F99.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using YkCalculator.Model;
+
+namespace YkCalculator.Logic
+{
+    public class F99 : FormulaBase, IFormula
+    {
+        public Output Calculate(Input input)
+        {
+            return CalculateReadyMadeProduct(input);
+        }
+    }
+}
